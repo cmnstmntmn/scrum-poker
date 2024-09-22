58e5d4e57f396916b2db34eed24373716eb940b8
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2024-09-22 23:53:21 +0300

    cleanup

--- lib/scrum_poker_web/live/home_live.ex
+++ lib/scrum_poker_web/live/home_live.ex
@@ -8,16 +8,11 @@ defmodule ScrumPokerWeb.HomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    if(connected?(socket)) do
-      Phoenix.PubSub.subscribe(ScrumPoker.PubSub, @topic);
-    end
+    if connected?(socket), do: Phoenix.PubSub.subscribe(ScrumPoker.PubSub, @topic)
 
     presences = Presence.list(@topic)
     socket = socket
-    |> assign(diff: nil)
-    |> assign(revealed: false)
-    |> assign(result: nil)
-    |> assign(room_id: nil)
+    |> assign(diff: nil, revealed: false, result: nil, room_id: nil)
     |> assign(current_user: nil, spectator: false, joined: false, vote: nil)
     |> assign(users: simple_presence_map(presences))
 
@@ -26,15 +21,11 @@ defmodule ScrumPokerWeb.HomeLive do
 
   @impl true
   def handle_event("join", %{"name" => name}, socket) do
-    userName = name |> String.trim() |> String.capitalize()
+    user_name = name |> String.trim() |> String.capitalize()
 
-    if(connected?(socket)) do
-      pid = self() |> :erlang.pid_to_list() |> List.to_string();
-      regex = ~r/<0\.(\d+)\.0>/
-      [_, pidId] = Regex.run(regex, pid)
-
-      {:ok, _} = Presence.track(self(), @topic, userName , %{
-        username: userName,
+    if connected?(socket) do
+      {:ok, _} = Presence.track(self(), @topic, user_name, %{
+        username: user_name,
         joined: true,
         voted: false,
         vote: ''
@@ -42,11 +33,12 @@ defmodule ScrumPokerWeb.HomeLive do
     end
 
     presences = Presence.list(@topic)
-    socket = assign(socket, joined: true, current_user: userName, users: simple_presence_map(presences))
+    socket = assign(socket, joined: true, current_user: user_name, users: simple_presence_map(presences))
 
     {:noreply, socket}
   end
 
+  @impl true
   def handle_event("update", %{"room" => room, "name" => name, "spectator" => spectator}, socket) do
     socket = assign(socket,
       room_id: return_string_or_nil(room),
@@ -57,27 +49,13 @@ defmodule ScrumPokerWeb.HomeLive do
     {:noreply, socket}
   end
 
-  defp return_string_or_nil(value) do
-    case value do
-      "" -> nil
-      _ -> value
-    end
-  end
-
-  defp return_bool_from_string(value) do
-    case value do
-      "true" -> true
-      "false" -> false
-    end
-  end
-
   def handle_event("select", %{"vote" => vote}, socket) do
     %{current_user: current_user} = socket.assigns
-    isDeselected = vote == socket.assigns.vote
-    voteValue = if isDeselected, do: '', else: vote
+    is_deselected = vote == socket.assigns.vote
+    vote_value = if is_deselected, do: '', else: vote
 
     %{metas: [meta | _]} = Presence.get_by_key(@topic, current_user)
-    new_meta = %{meta | voted: !isDeselected, vote: voteValue}
+    new_meta = %{meta | voted: !is_deselected, vote: vote_value}
     Presence.update(self(), @topic, current_user, new_meta)
 
     users_count = socket.assigns.users |> Map.size()
@@ -87,30 +65,12 @@ defmodule ScrumPokerWeb.HomeLive do
 
     Phoenix.PubSub.broadcast(ScrumPoker.PubSub, @topic, %{event: "result", payload: result})
 
-    socket = assign(socket, vote: voteValue, result: result)
+    socket = assign(socket, vote: vote_value, result: result)
     {:noreply, socket}
   end
 
-  defp get_result(users_count, vote_count, votes) do
-    case users_count === vote_count do
-      false -> nil
-      _ -> find_most_frequent(votes)
-    end
-  end
-
-  def find_most_frequent(list) do
-    frequencies = Enum.frequencies(list)
-
-    list
-    |> Enum.max_by(fn item -> frequencies[item] end)
-  end
-
-  defp simple_presence_map(presences) do
-    Enum.into(presences, %{}, fn {user_id, %{metas: [meta | _]}} -> {user_id, meta} end)
-  end
-
-  def handle_info(%{event: "result", payload: vote}, socket) do
-    socket = assign(socket, result: vote)
+  def handle_info(%{event: "result", payload: result}, socket) do
+    socket = assign(socket, result: result)
     {:noreply, socket}
   end
 
@@ -123,9 +83,28 @@ defmodule ScrumPokerWeb.HomeLive do
     {:noreply, socket}
   end
 
+  defp return_string_or_nil(""), do: nil
+  defp return_string_or_nil(value), do: value
+
+  defp return_bool_from_string("true"), do: true
+  defp return_bool_from_string("false"), do: false
+
+  defp get_result(users_count, vote_count, votes) when users_count == vote_count, do: find_most_frequent(votes)
+  defp get_result(_, _, _), do: nil
+
+  defp find_most_frequent(list) do
+    list
+    |> Enum.frequencies()
+    |> Enum.max_by(fn {_item, count} -> count end)
+    |> elem(0)
+  end
+
+  defp simple_presence_map(presences) do
+    Enum.into(presences, %{}, fn {user_id, %{metas: [meta | _]}} -> {user_id, meta} end)
+  end
+
   defp remove_presences(socket, leaves) do
     user_ids = Enum.map(leaves, &elem(&1, 0))
-
     presences = Map.drop(socket.assigns.users, user_ids)
     assign(socket, :users, presences)
   end
